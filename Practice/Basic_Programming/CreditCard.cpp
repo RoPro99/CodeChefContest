22159793ba04d3a8f3809e3e0b8914494dd9b6c9
RoPro99 <rohankodag499@gmail.com>
2022-09-23 10:14:53 +0530

    Basic Programming

---
+++ Practice/Basic_Programming/CreditCard.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main() {
+	int t, x;
+	cin >> t;
+	while (t--) {
+		cin >> x;
+		if (x >= 750) {
+			cout << "Yes" << endl;
+
+		}
+		else {
+			cout << "No" << endl;
+		}
+	}
+	return 0;
+}
---
+++ Practice/Basic_Programming/IncreaseIQ.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main() {
+	int x;
+	cin >> x;
+	int res = x + 7;
+	if (res > 170) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+	return 0;
+}
